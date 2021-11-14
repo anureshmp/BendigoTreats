c05eb4abd959877cf96a2f20b4c340af49f9e8b4
anureshmp <anureshmp@gmail.com>
2021-11-14 16:43:05 +1100

    Added Infrastructure project | Added repository interface and context class

---
+++ BendigoTreats.Infrastructure/ShoppingContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BendigoTreats.Domain;
+using BendigoTreats.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BendigoTreats.Infrastructure
+{
+	public class ShoppingContext:DbContext
+	{
+
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+		public ShoppingContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+		{
+
+		}
+
+
+	}
+}
